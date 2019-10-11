3ebddb786a71b201ffe9ab96d3e77922bbc6fecd
Aexp <amex@AEXPs-MacBook-Pro.local>
2019-10-11 14:29:05 +0100

    wired up timezones

--- AgentDiscoveries-Frontend/app/src/components/todays-code-page.jsx
+++ AgentDiscoveries-Frontend/app/src/components/todays-code-page.jsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import {Button, ControlLabel, Form, FormControl, FormGroup} from 'react-bootstrap';
 import {apiPost} from './utilities/request-helper';
+import {SlideDown} from 'react-slidedown';
+import moment from 'moment-timezone';
+import 'moment-timezone';
 
 export default class TodaysCodePage extends React.Component {
     constructor(props) {
@@ -8,16 +11,26 @@ export default class TodaysCodePage extends React.Component {
 
         this.state = {
             message: '',
-            result: ''
+            result: '',
+            showMessage: false
+
         };
 
         this.onChange = this.onChange.bind(this);
         this.handleDecode = this.handleDecode.bind(this);
         this.handleEncode = this.handleEncode.bind(this);
         this.handleRequest = this.handleRequest.bind(this);
+        this.slideDown = this.slideDown.bind(this);
+
     }
 
     render() {
+           var london = moment.tz("Europe/London");
+           var losAngeles = london.clone().tz("America/Los_Angeles");
+           var newYork = london.clone().tz("America/New_York");
+           var tokyo = london.clone().tz("Asia/Tokyo");
+           var moscow = london.clone().tz("Europe/Moscow");
+
         return (
             <div className="container">
 
@@ -27,40 +40,45 @@ export default class TodaysCodePage extends React.Component {
 
                 <div className="row">
                     <div className="col-sm-2 col-sm-offset-1 text-center timezone">
-                        <h2><span>NEW YORK</span><br/>18:00</h2>
+                        <h2><span>NEW YORK</span><br/>{newYork.format('H:mm')}</h2>
                     </div>
                     <div className="col-sm-2 text-center timezone">
-                        <h2><span>LONDON</span><br/>18:00</h2>
+                        <h2><span>LONDON</span><br/>{london.format('H:mm')}</h2>
                     </div>
                     <div className="col-sm-2 text-center timezone">
-                        <h2><span>LOS ANGELES</span><br/>18:00</h2>
+                        <h2><span>LOS ANGELES</span><br/>{losAngeles.format('H:mm')}</h2>
                     </div>
                     <div className="col-sm-2 text-center timezone">
-                        <h2><span>TOKYO</span><br/>18:00</h2>
+                        <h2><span>TOKYO</span><br/>{tokyo.format('H:mm')}</h2>
                     </div>
                     <div className="col-sm-2 text-center timezone">
-                        <h2><span>MOSCOW</span><br/>18:00</h2>
+                        <h2><span>MOSCOW</span><br/>{moscow.format('H:mm')}</h2>
                     </div>
                 </div>
 
                 <div className='col-md-8 col-md-offset-2 text-center'>
-                    <Button className="message-btn">Encode Message</Button>
-                    <Form>
-                        <h3>Encode/decode message with today's secret</h3>
 
-                        <FormGroup>
-                            <ControlLabel>Message</ControlLabel>
-                            <FormControl type='text' required
-                                id='message-input'
-                                componentClass='textarea' rows={6}
-                                placeholder='Enter message'
-                                value={this.state.message}
-                                onChange={this.onChange}/>
-                        </FormGroup>
+                    <Button className="message-btn" onClick={this.slideDown}>Encode Message</Button>
 
-                        <Button id="encode-button" className='rm-3' type='submit' onClick={this.handleEncode}>Encode</Button>
-                        <Button id="decode-button" type='submit' onClick={this.handleDecode}>Decode</Button>
-                    </Form>
+                    <SlideDown className={'my-dropdown-slidedown'}>
+                        {this.state.showMessage ?
+                        <Form>
+                            <h3>Encode/decode message with today's secret</h3>
+
+                            <FormGroup>
+                                <ControlLabel>Message</ControlLabel>
+                                <FormControl type='text' required
+                                    id='message-input'
+                                    componentClass='textarea' rows={6}
+                                    placeholder='Enter message'
+                                    value={this.state.message}
+                                    onChange={this.onChange}/>
+                            </FormGroup>
+
+                            <Button id="encode-button" className='rm-3' type='submit' onClick={this.handleEncode}>Encode</Button>
+                            <Button id="decode-button" type='submit' onClick={this.handleDecode}>Decode</Button>
+                        </Form> : null}
+                    </SlideDown>
 
 
                     <div id="code-result">
@@ -71,6 +89,14 @@ export default class TodaysCodePage extends React.Component {
             </div>
 
         );
+    }
+
+    hide(event) {
+        this.setState({hideButton: false})
+    }
+
+    slideDown(event) {
+        this.setState({showMessage: true})
     }
 
     onChange(event) {
